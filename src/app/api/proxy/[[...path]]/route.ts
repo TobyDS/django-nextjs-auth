134cca0f82cde770263f59221891cdfae84dfcdb
Toby Dixon Smith <tobydixonsmith@gmail.com>
2024-11-21 17:05:22 +0000

    feat: support all methods in proxy

--- src/app/api/proxy/[[...path]]/route.ts
+++ src/app/api/proxy/[[...path]]/route.ts
@@ -1,45 +1,44 @@
 import { auth } from '@/auth';
 import { headers } from 'next/headers';
 import { NextRequest } from 'next/server';
+import { DJANGO_API_BASE_URL } from '@/utils/constants';
 
-const authorizeRequest = async (req: NextRequest, method = 'GET') => {
+const authorizeRequest = async (req: NextRequest) => {
   const session = await auth();
   if (!session?.accessToken) {
     return new Response('Unauthorized', { status: 401 });
   }
+
   const path = req.nextUrl.pathname.replace('/api/proxy', '');
+  const formattedPath = path.endsWith('/') ? path : `${path}/`;
   const searchParams = req.nextUrl.searchParams.toString();
 
-  // Ensure the path ends with a trailing slash if necessary
-  const formattedPath = path.endsWith('/') ? path : `${path}/`;
-
-  // Get the body correctly
   let body = null;
   if (req.body) {
     const text = await req.text();
-    body = text; // Don't stringify again, it's already a string
+    body = text;
   }
 
-  const url = `http://localhost:8000${formattedPath}${searchParams ? '?' + searchParams : ''}`;
+  const url = `${DJANGO_API_BASE_URL}${formattedPath}${searchParams ? '?' + searchParams : ''}`;
   const headersList = headers();
-  const response = await fetch(url, {
+
+  return fetch(url, {
     headers: {
       Authorization: `Bearer ${session.accessToken}`,
       'Content-Type':
         (await headersList).get('content-type') || 'application/json',
     },
-    method,
+    method: req.method,
     body,
   });
-  return response;
 };
 
-export async function GET(req: NextRequest) {
-  const response = await authorizeRequest(req);
-  return response;
-}
-
-export async function PUT(req: NextRequest) {
-  const response = await authorizeRequest(req, 'PUT');
-  return response;
-}
+// Handle all methods
+export const GET = authorizeRequest;
+export const PUT = authorizeRequest;
+export const POST = authorizeRequest;
+export const DELETE = authorizeRequest;
+export const PATCH = authorizeRequest;
+export const OPTIONS = authorizeRequest;
+export const HEAD = authorizeRequest;
+export const TRACE = authorizeRequest;
